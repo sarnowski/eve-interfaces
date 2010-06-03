38b3a7849add59a3b7159e0a3cc0958b69c04be9
Tobias Sarnowski <sarnowski@new-thoughts.org>
2010-06-03 21:12:12 +0200

    added SovereigntyStatusApi

---
+++ src/main/java/com/eveonline/api/map/SovereigntyStatusApi.java
@@ -0,0 +1,14 @@
+/**
+ * (c) 2010 Tobias Sarnowski
+ * All rights reserved.
+ */
+package com.eveonline.api.map;
+
+import com.eveonline.api.ApiService;
+
+/**
+ * @author Tobias Sarnowski
+ */
+public interface SovereigntyStatusApi extends ApiService {
+	
+}
