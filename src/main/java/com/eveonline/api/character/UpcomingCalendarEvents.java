3a7e290cd3965e0954b9353f6eae951ede475b5f
Tobias Sarnowski <sarnowski@new-thoughts.org>
2010-06-03 19:01:11 +0200

    added dummy UpcomingCalendarEvents

---
+++ src/main/java/com/eveonline/api/character/UpcomingCalendarEvents.java
@@ -0,0 +1,14 @@
+/**
+ * (c) 2010 Tobias Sarnowski
+ * All rights reserved.
+ */
+package com.eveonline.api.character;
+
+/**
+ * @author Tobias Sarnowski
+ */
+public interface UpcomingCalendarEvents {
+
+	// TODO
+
+}
