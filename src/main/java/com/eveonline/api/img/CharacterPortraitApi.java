03e7b8fa469750e9b55f67acf58729a0f98ad70d
Tobias Sarnowski <sarnowski@cosmocode.de>
2010-06-15 17:38:43 +0200

    fixed typo

--- src/main/java/com/eveonline/api/img/CharacterPortraitApi.java
+++ src/main/java/com/eveonline/api/img/CharacterPortraitApi.java
@@ -28,7 +28,7 @@ public interface CharacterPortraitApi extends ApiService {
 
 	/**
 	 * @param characterId the character's ID
-	 * @param size Size can be 64x64px or 256x264px
+	 * @param size Size can be 64x64px or 256x256px
 	 * @return a JPEG encoded image of the character
 	 * @throws ApiException if an error occurs
 	 */
